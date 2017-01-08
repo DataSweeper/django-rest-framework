808d5236fe7f301ca9d4629ff7c1ff0541c10813
siva <sivagnanam.a@zohocorp.com>
2017-01-08 22:00:23 +0530

    model change

--- rest_framework/authtoken/models.py
+++ rest_framework/authtoken/models.py
@@ -13,10 +13,17 @@ class Token(models.Model):
     The default authorization token model.
     """
     key = models.CharField(_("Key"), max_length=250, primary_key=True)
-    user = models.OneToOneField(
+
+    """user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='auth_token',
+        on_delete=models.CASCADE, verbose_name=_("User")
+    )"""
+
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='auth_token',
         on_delete=models.CASCADE, verbose_name=_("User")
     )
+
     created = models.DateTimeField(_("Created"), auto_now_add=True)
 
     class Meta:
